8c2c4767f5c8582912df76a8b12ed332d5f3ab0a
Kuba429 <jakubzawadzki429@gmail.com>
2022-04-12 20:37:58 +0200

    specific location view

--- src/views/SpecificLocation.vue
+++ src/views/SpecificLocation.vue
@@ -1,7 +1,43 @@
 <template>
-    <div>Location</div>
+    <div v-if="location">
+        <h1>{{ location.name }}</h1>
+        <p>
+            Type: <span>{{ location.type }}</span>
+        </p>
+        <p>
+            Dimension: <span>{{ location.dimension }}</span>
+        </p>
+        <h3>Residents:</h3>
+        <CharacterContainer :characters="characters" />
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from "axios";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import { Character, Location } from "../Interfaces";
+import CharacterContainer from "../components/CharacterContainer.vue";
+const { id } = useRoute().params;
+const location = ref<Location>();
+const characters = ref<Character[]>([]);
+onMounted(async () => {
+    const response = await axios.get(
+        "https://rickandmortyapi.com/api/location/" + id
+    );
+    location.value = response.data;
+    const charactersIDs = response.data.residents.map(
+        (resident: string) => resident.split("/")[5]
+    );
+    const responseCharacters = await axios.get(
+        "https://rickandmortyapi.com/api/character/" + charactersIDs
+    );
+    characters.value = Array.isArray(responseCharacters.data)
+        ? responseCharacters.data
+        : [responseCharacters.data];
+});
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import "../variables.scss";
+</style>
